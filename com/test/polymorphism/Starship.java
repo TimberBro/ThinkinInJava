935f1423fd3c60f8919d17f9991a2cd7a478f9f7
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-03-07 22:34:00 +0300

    Chapter 9 - Exercise 16.

--- com/test/polymorphism/Starship.java
+++ com/test/polymorphism/Starship.java
@@ -1,48 +1,52 @@
 package com.test.polymorphism;
 
 class Alert {
-    public void getStatus() {}
-}
-class GreenAlert extends Alert{
-    public void getStatus(){
-        System.out.println("System is fine!");
-    }
+  public void getStatus() {}
 }
 
-class YellowAlert extends Alert{
-    public void getStatus(){
-        System.out.println("System contains some warnings!");
-    }
+class GreenAlert extends Alert {
+  public void getStatus() {
+    System.out.println("System is fine!");
+  }
 }
 
-class RedAlert extends Alert{
-    public void getStatus(){
-        System.out.println("SYSTEM IS ON FIRE! FIX IT!");
-    }
+class YellowAlert extends Alert {
+  public void getStatus() {
+    System.out.println("System contains some warnings!");
+  }
+}
+
+class RedAlert extends Alert {
+  public void getStatus() {
+    System.out.println("SYSTEM IS ON FIRE! FIX IT!");
+  }
 }
 
 class Incident {
-    private Alert alert = new GreenAlert();
-    public void overheat(){
-        alert = new YellowAlert();
-    }
-    public void shortcircuit(){
-        alert = new RedAlert();
-    }
-    public void printStatus(){
-        alert.getStatus();
-    }
+  private Alert alert = new GreenAlert();
+
+  public void overheat() {
+    alert = new YellowAlert();
+  }
+
+  public void shortCircuit() {
+    alert = new RedAlert();
+  }
+
+  public void printStatus() {
+    alert.getStatus();
+  }
 }
 
 public class Starship {
-    public static void main(String[] args){
-        Incident incident = new Incident();
-        incident.printStatus();
-        System.out.println("Before overheat");
-        incident.overheat();
-        incident.printStatus();
-        System.out.println("Before short circuit");
-        incident.shortcircuit();
-        incident.printStatus();
-    }
+  public static void main(String[] args) {
+    Incident incident = new Incident();
+    incident.printStatus();
+    System.out.println("Before overheat");
+    incident.overheat();
+    incident.printStatus();
+    System.out.println("Before short circuit");
+    incident.shortCircuit();
+    incident.printStatus();
+  }
 }
