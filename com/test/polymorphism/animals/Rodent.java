2e2cdd06df1ca2fc504b465ebbfeb2f4edd40d92
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-03-07 18:31:51 +0300

    Chapter 9 - Exercise 12.

--- com/test/polymorphism/animals/Rodent.java
+++ com/test/polymorphism/animals/Rodent.java
@@ -1,12 +1,23 @@
 package com.test.polymorphism.animals;
 
+class Tail {
+  public int length;
+
+  Tail() {
+    System.out.println("Tail constructor");
+  }
+}
+
 public class Rodent {
   void type() {
     System.out.println("This is base type");
   }
 
+  private Tail tail = new Tail();
+
   Rodent() {
     System.out.println("Rodent constructor");
+    tail.length = 15;
   }
 
   public static void main(String[] args) {
@@ -20,31 +31,40 @@ public class Rodent {
 }
 
 class Mouse extends Rodent {
+  private Tail tail = new Tail();
+
   void type() {
     System.out.println("This is mouse");
   }
 
   Mouse() {
     System.out.println("Mouse constructor");
+    tail.length = 5;
   }
 }
 
 class Gerbil extends Rodent {
+  private Tail tail = new Tail();
+
   void type() {
     System.out.println("This is gerbil");
   }
 
   Gerbil() {
     System.out.println("Gerbil constructor");
+    tail.length = 2;
   }
 }
 
 class Hamster extends Rodent {
+  private Tail tail = new Tail();
+
   void type() {
     System.out.println("This is hamster");
   }
 
   Hamster() {
     System.out.println("Hamster constructor");
+    tail.length = 1;
   }
 }
