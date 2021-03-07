8abe0c51ba099b4528e764f53f656b4ed1b2b101
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-03-07 22:29:35 +0300

    Chapter 9 - Exercise 15.

--- com/test/polymorphism/ReferenceCounting.java
+++ com/test/polymorphism/ReferenceCounting.java
@@ -1,37 +1,5 @@
 package com.test.polymorphism;
 
-class Shared {
-  private static long counter = 0;
-  private final long id = counter++;
-  private int refcount = 0;
-
-  public Shared() {
-    System.out.println("Creating " + this);
-  }
-
-  public void addRef() {
-    refcount++;
-  }
-
-  protected void dispose() {
-    if (--refcount == 0)
-      System.out.println("Disposing " + this);
-  }
-
-  public String toString() {
-    return "Shared " + id;
-  }
-
-  protected void finalize() {
-    if (refcount > 0) {
-      System.out.println("Error: Shared with id = " + id + " is in use.");
-    }
-    else {
-      System.out.println("The Shared object cleaned up.");
-    }
-  }
-}
-
 class Composing {
   private static long counter = 0;
   private final Shared shared;
