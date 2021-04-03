d795d2560167d1d64e9e8c95427f989a3f4219c1
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-04-03 17:58:30 +0300

    Chapter 11 - Exercise 9.

---
+++ com/test/innerclasses/InnerClassReturnInterface.java
@@ -0,0 +1,24 @@
+package com.test.innerclasses;
+
+interface Airplane {
+  void fly();
+}
+
+public class InnerClassReturnInterface {
+
+  Airplane getAirplane() {
+    class Airport implements Airplane {
+      @Override
+      public void fly() {
+        System.out.println("Flying away!");
+      }
+    }
+
+    return new Airport();
+  }
+
+  public static void main(String[] args) {
+    InnerClassReturnInterface innerClassReturnInterface = new InnerClassReturnInterface();
+    innerClassReturnInterface.getAirplane().fly();
+  }
+}
