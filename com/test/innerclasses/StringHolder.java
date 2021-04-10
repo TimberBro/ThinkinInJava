25558e96c3bc01ed7c01777e031ecc39dee89fe5
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-04-10 19:18:24 +0300

    Chapter 11 - Exercise 22.

--- com/test/innerclasses/StringHolder.java
+++ com/test/innerclasses/StringHolder.java
@@ -32,8 +32,33 @@ class Sequence {
     }
   }
 
+  private class ReverseSelector implements Selector {
+    private int i = items.length;
+
+    @Override
+    public boolean end() {
+      return i == 0;
+    }
+
+    @Override
+    public Object current() {
+      return items[i-1];
+    }
+
+    @Override
+    public void next() {
+      if (i > 0) {
+        i--;
+      }
+    }
+  }
+
   public Selector selector() {
     return new SequenceSelector();
+  }
+
+  public Selector reverseSelector() {
+    return new ReverseSelector();
   }
 }
 
@@ -54,10 +79,17 @@ public class StringHolder {
     sequence.add(new StringHolder("Hello!"));
     sequence.add(new StringHolder("Привет!"));
     sequence.add(new StringHolder("Hallo!"));
+    System.out.println("Your array is:");
     Selector selector = sequence.selector();
     while (!selector.end()) {
       System.out.println(selector.current() + " ");
       selector.next();
+    }
+    System.out.println("Now array will be printed backwards:");
+    Selector reverseSelector = sequence.reverseSelector();
+    while (!reverseSelector.end()) {
+      System.out.println(reverseSelector.current() + " ");
+      reverseSelector.next();
     }
   }
 }
