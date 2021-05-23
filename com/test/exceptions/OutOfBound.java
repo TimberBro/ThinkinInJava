294f28886ecd7e80390420d9a1a613397a15b5ef
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-05-23 12:14:56 +0300

    Chapter 13 - Exercise 3.

---
+++ com/test/exceptions/OutOfBound.java
@@ -0,0 +1,14 @@
+package com.test.exceptions;
+
+public class OutOfBound {
+  public static void main(String[] args) {
+    int[] ints = new int[5];
+    try {
+      for (int i = 0; i < 6; i++) {
+        ints[i] = i;
+      }
+    } catch (IndexOutOfBoundsException e) {
+      e.printStackTrace();
+    }
+  }
+}
