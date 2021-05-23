e062464e1a3505a12b56cc70e55cc2ec64e5d6d2
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-05-23 13:52:28 +0300

    Chapter 13 - Exercise 7.

--- com/test/exceptions/OutOfBound.java
+++ com/test/exceptions/OutOfBound.java
@@ -1,6 +1,19 @@
 package com.test.exceptions;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+
 public class OutOfBound {
+  private static Logger logger = Logger.getLogger("LoggedIndexOutOfBoundException");
+
+  static void logException(Exception e) {
+    StringWriter trace = new StringWriter();
+    e.printStackTrace(new PrintWriter(trace));
+    logger.severe(trace.toString());
+  }
+
   public static void main(String[] args) {
     int[] ints = new int[5];
     try {
@@ -8,7 +21,7 @@ public class OutOfBound {
         ints[i] = i;
       }
     } catch (IndexOutOfBoundsException e) {
-      e.printStackTrace();
+      logException(e);
     }
   }
 }
