7657f70fbcecfe3515b2a0973cc154bbe3fc304b
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-06-04 12:47:48 +0300

    Chapter 13 - Exercise 23.

--- com/test/exceptions/FailingConstructor.java
+++ com/test/exceptions/FailingConstructor.java
@@ -2,13 +2,37 @@ package com.test.exceptions;
 
 class IncorrectName extends Exception {}
 
-class FailingConstructor {
+class DisposableObject {
+  private static int counter;
+  private final int id;
+
+  DisposableObject() {
+    id = ++counter;
+  }
+
+  void dispose() {
+    System.out.println("Object " + id + " was disposed.");
+    counter--;
+  }
+}
+
+public class FailingConstructor {
   private String name;
 
+  DisposableObject disposable1;
+  DisposableObject disposable2;
+
   FailingConstructor(String name, boolean correctName) throws IncorrectName {
-    if (correctName) {
-      throw new IncorrectName();
+    disposable1 = new DisposableObject();
+    try {
+      if (correctName) {
+        throw new IncorrectName();
+      }
+    } catch (IncorrectName e) {
+      disposable1.dispose();
+      throw e;
     }
+    disposable2 = new DisposableObject();
   }
 
   public static void main(String[] args) {
