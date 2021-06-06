ec7792f078976d608c53d700cee03c406bd25069
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-06-06 20:12:38 +0300

    Chapter 13 - Exercise 29.

--- com/test/exceptions/StormyInning.java
+++ com/test/exceptions/StormyInning.java
@@ -1,90 +1,53 @@
 package com.test.exceptions;
 
-class BaseballException extends Exception {}
+class BaseballException extends RuntimeException {}
 
 class Foul extends BaseballException {}
 
 class Strike extends BaseballException {}
 
-class UmpireArgument extends BaseballException {}
-
 abstract class Inning {
-  public Inning() throws BaseballException {}
+  public Inning() {}
 
-  public void event() throws BaseballException {
-    // Doesn't actually have to throw anything
-  }
+  public void event() {}
 
-  public abstract void atBat() throws Strike, Foul, UmpireArgument;
+  public abstract void atBat();
 
-  public void walk() {} // Throws no checked exceptions
+  public void walk() {}
 }
 
-class StormException extends Exception {}
+class StormException extends RuntimeException {}
 
 class RainedOut extends StormException {}
 
 class PopFoul extends Foul {}
 
 interface Storm {
-  public void event() throws RainedOut;
+  public void event();
 
-  public void rainHard() throws RainedOut;
+  public void rainHard();
 }
 
 public class StormyInning extends Inning implements Storm {
-  // OK to add new exceptions for constructors, but you
-  // must deal with the base constructor exceptions:
-  public StormyInning() throws RainedOut, BaseballException {}
+  public StormyInning() {}
 
-  public StormyInning(String s) throws Foul, BaseballException {}
-  // Regular methods must conform to base class:
-  // ! void walk() throws PopFoul {} //Compile error
-  // Interface CANNOT add exceptions to existing
-  // methods from the base class:
-  // ! public void event() throws RainedOut {}
-  // If the method doesn't already exist in the
-  // base class, the exception is OK:
+  public StormyInning(String s) {}
 
-  public void rainHard() throws RainedOut {}
-  // You can choose to not throw any exceptions,
-  // even if the base version does:
+  public void walk() {} // This line was commented, because of compile error.
+
+  public void rainHard() {}
 
   public void event() {}
-  // Overridden methods can throw inherited exceptions:
 
-  public void atBat() throws PopFoul, UmpireArgument {
-    throw new UmpireArgument();
+  public void atBat() {
+    // Added throw for proof of work.
+    throw new PopFoul();
   }
 
   public static void main(String[] args) {
-    try {
-      StormyInning si = new StormyInning();
-      si.atBat();
-    } catch (PopFoul e) {
-      System.out.println("Pop foul");
-    } catch (RainedOut e) {
-      System.out.println("Rained out");
-    } catch (UmpireArgument e) {
-      System.out.println("Argument with umpire.");
-    } catch (BaseballException e) {
-      System.out.println("Generic baseball exception");
-    }
-    // Strike not thrown in derived version.
-    try {
-      // What happens if you upcast?
-      Inning i = new StormyInning();
-      i.atBat();
-      // You must catch the exceptions from the
-      // base-class version of the method:
-    } catch (Strike e) {
-      System.out.println("Strike");
-    } catch (Foul e) {
-      System.out.println("Foul");
-    } catch (RainedOut e) {
-      System.out.println("Rained out");
-    } catch (BaseballException e) {
-      System.out.println("Generic baseball exception");
-    }
+    StormyInning si = new StormyInning();
+    si.atBat();
+    Inning i = new StormyInning();
+    i.atBat();
   }
 }
