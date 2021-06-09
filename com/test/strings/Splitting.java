aba13882a80e2ac27c8b7a8c979928a0b7392541
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-06-09 21:10:35 +0300

    Chapter 14 - Exercise 8.

--- com/test/strings/Splitting.java
+++ com/test/strings/Splitting.java
@@ -7,10 +7,11 @@ public class Splitting {
       "Then, when you have found the shrubbery, you must "
           + "cut down the mightiest tree in the forest... "
           + "with... a herring!";
+
   public static void split(String regex) {
-    System.out.println(
-        Arrays.toString(knights.split(regex)));
+    System.out.println(Arrays.toString(knights.split(regex)));
   }
+
   public static void main(String[] args) {
     split("you|the");
   }
