88e5992387eb7a8e3b2eb906c49f42d5a7212e21
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-06-12 14:39:20 +0300

    Chapter 14 - Exercise 12.

--- com/test/strings/Groups.java
+++ com/test/strings/Groups.java
@@ -8,13 +8,13 @@ import java.util.regex.Pattern;
 public class Groups {
   public static final String POEM =
       "Twas brillig, and the slithy toves\n"
-          + "Did gyre and gimble in the wabe.Xn"
-          + "All mimsy were the borogoves, \n"
-          + "And the mome raths outgrabe. \n\n"
+          + "Did gyre and gimble in the wabe.\n"
+          + "All mimsy were the borogoves,\n"
+          + "And the mome raths outgrabe.\n\n"
           + "Beware the Jabberwock, my son,\n"
           + "The jaws that bite, the claws that catch.\n"
           + "Beware the Jubjub bird, and shun\n"
-          + "The frumious Bandersnatch .";
+          + "The frumious Bandersnatch.";
 
   public static void main(String[] args) {
     // Using set to store unique values
