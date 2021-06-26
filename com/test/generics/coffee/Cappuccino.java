1b15581cd08fa8156d68e14fbf3246d7558bb49a
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-06-26 19:05:01 +0300

    Chapter 15 - Exercise 16. Random fixed, issue caused by coffee type order.

--- com/test/generics/coffee/Cappuccino.java
+++ com/test/generics/coffee/Cappuccino.java
@@ -1,3 +1,8 @@
 package com.test.generics.coffee;
 
-public class Cappuccino extends Coffee {}
+public class Cappuccino extends Coffee {
+  public static class Factory
+      implements com.test.typeinfo.factory.Factory<Cappuccino> {
+    public Cappuccino create() { return new Cappuccino(); }
+  }
+}
--- com/test/generics/coffee/Coffee.java
+++ com/test/generics/coffee/Coffee.java
@@ -1,10 +1,31 @@
 package com.test.generics.coffee;
 
+import com.test.typeinfo.factory.Factory;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Coffee {
   private static long counter = 0;
   private final long id = counter++;
 
   public String toString() {
     return getClass().getSimpleName() + " " + id;
+  }
+
+  public static List<Factory<? extends Coffee>> coffeeFactories =
+      Arrays.asList(
+          new Latte.Factory(),
+          new Mocha.Factory(),
+          new Cappuccino.Factory(),
+          new Americano.Factory(),
+          new Breve.Factory()
+          );
+
+  private static Random rand = new Random(47);
+
+  public static Coffee createRandom() {
+    int n = rand.nextInt(coffeeFactories.size());
+    return coffeeFactories.get(n).create();
   }
 }
