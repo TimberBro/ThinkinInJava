81f74212a4376bce869475372318da0d11291dff
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-07-08 23:30:44 +0300

    Chapter 16 - Exercise 12.

--- com/test/generics/NewTest.java
+++ com/test/generics/NewTest.java
@@ -7,14 +7,22 @@ import java.util.List;
 import java.util.Set;
 
 public class NewTest {
-  public static void main(String[] args) {
-    List<StoryCharacter> storyCharactersList = New.list();
+
+  static void f(List<StoryCharacter> storyCharactersList) {
     for (StoryCharacter character : new StoryCharacterGenerator(5)) {
       storyCharactersList.add(character);
     }
     System.out.println(storyCharactersList);
-    Set<Fibonacci> fibonacciSet = New.set();
+  }
+
+  static void g(Set<Fibonacci> fibonacciSet) {
     fibonacciSet.add(new Fibonacci());
     System.out.println(fibonacciSet);
+  }
+
+  public static void main(String[] args) {
+    f(New.<StoryCharacter>list());
+    g(New.<Fibonacci>set());
+    // Output will be same.
   }
 }
