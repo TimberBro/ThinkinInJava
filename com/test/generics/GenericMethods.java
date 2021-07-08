8e68f637d625cb21fe4b78eacbcb3c06593eb6e6
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-07-08 22:07:23 +0300

    Chapter 16 - Exercise 10.

--- com/test/generics/GenericMethods.java
+++ com/test/generics/GenericMethods.java
@@ -1,7 +1,7 @@
 package com.test.generics;
 
 public class GenericMethods {
-  public <T, U, V> void f(T x, U y, V z) {
+  public <T, U> void f(T x, U y, GenericMethods z) {
     System.out.println(x.getClass().getName());
     System.out.println(y.getClass().getName());
     System.out.println(z.getClass().getName());
@@ -9,7 +9,7 @@ public class GenericMethods {
 
   public static void main(String[] args) {
     GenericMethods gm = new GenericMethods();
-    gm.f("", 1, 1.0);
+    gm.f("", 1, gm);
     gm.f(1.0F, 'c', gm);
   }
 }
