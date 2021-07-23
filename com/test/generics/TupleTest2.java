93f807efe30a43f6e3891eef725e7de31467725a
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-07-23 14:21:26 +0300

    Chapter 16 - Exercise 15.

---
+++ com/test/generics/TupleTest2.java
@@ -0,0 +1,41 @@
+package com.test.generics;
+
+import static com.test.util.Tuple.tuple;
+
+import com.test.util.FiveTuple;
+import com.test.util.FourTuple;
+import com.test.util.ThreeTuple;
+import com.test.util.TwoTuple;
+
+public class TupleTest2 {
+  static TwoTuple<String, Integer> f() {
+    return tuple("hi", 47);
+  }
+
+  static TwoTuple f2() {
+    return tuple("hi", 47);
+  }
+
+  static ThreeTuple<Amphibian, String, Integer> g() {
+    return tuple(new Amphibian(), "hi", 47);
+  }
+
+  static FourTuple<Vehicle, Amphibian, String, Integer> h() {
+    return tuple(new Vehicle(), new Amphibian(), "hi", 47);
+  }
+
+  static FiveTuple<Vehicle, Amphibian, String, Integer, Double> k() {
+    return tuple(new Vehicle(), new Amphibian(), "hi", 47, 11.1);
+  }
+
+  public static void main(String[] args) {
+    TwoTuple<String, Integer> ttsi = f();
+    System.out.println(ttsi);
+    // Same output, but compiler doesn't warn about issue.
+    TwoTuple<String, Integer> stringIntegerTwoTuple = f2();
+    System.out.println(stringIntegerTwoTuple);
+    System.out.println(g());
+    System.out.println(h());
+    System.out.println(k());
+  }
+}
