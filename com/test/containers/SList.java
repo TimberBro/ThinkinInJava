b31b42b080d58181455e1e4db2ea7f18f7dce8ff
Evgeniy Popov <mr.demix@yandex.ru>
2021-09-06 01:23:02 +0300

    Chapter 19 - Exercise 8

--- com/test/containers/SList.java
+++ com/test/containers/SList.java
@@ -1,15 +1,5 @@
 package com.test.containers;
 
-interface SIterator<T> {
-  boolean hasNext();
-
-  Link<T> next();
-
-  void remove();
-
-  void add(T object);
-}
-
 class Link<T> {
   T storedObject;
   Link<T> nextObject;
@@ -22,69 +12,83 @@ class Link<T> {
   Link(T object) {
     this(object, null);
   }
+
+  public Link<T> next() {
+    return nextObject;
+  }
+
+  public T getObject() {
+    return storedObject;
+  }
+
+  public String toString() {
+    if(storedObject == null) return "null";
+    return storedObject.toString();
+  }
+
+  public void setNext(Link<T> object) {
+    nextObject = object;
+  }
 }
 
 public class SList<T> {
 
   private final Link<T> header = new Link<>(null);
 
-  SList() {
-    header.nextObject = header;
-  }
-
   public String toString() {
-    if (header.nextObject == null) return "SList: []";
-    System.out.println("SList: [");
+    if (header.next() == null) return "List is empty";
     SIterator<T> it = this.iterator();
     StringBuilder s = new StringBuilder();
     while (it.hasNext()) {
       s.append(it.next() + (it.hasNext() ? ", " : ""));
     }
-    return s + "]";
-  }
-
-  private class SIteratorImpl implements SIterator<T> {
-
-    Link<T> currentElement;
-
-    SIteratorImpl(Link<T> link) {
-      currentElement = link;
-    }
-
-    @Override
-    public boolean hasNext() {
-      return currentElement.nextObject != null;
-    }
-
-    @Override
-    public Link<T> next() {
-      currentElement = currentElement.nextObject;
-      return currentElement;
-    }
-
-    @Override
-    public void remove() {
-      if (currentElement.nextObject != null) {
-        currentElement.nextObject = currentElement.nextObject.nextObject;
-      }
-    }
-
-    @Override
-    public void add(T object) {
-      currentElement.nextObject = new Link<>(object, currentElement.nextObject);
-      currentElement = currentElement.nextObject;
-    }
+    return s.toString();
   }
 
   public SIterator<T> iterator() {
-    return new SIteratorImpl(header);
+    return new SIterator<T>(header);
   }
-  // TODO: 03.09.2021 Implement SListIterator class
+}
+
+class SIterator<T> {
+
+  Link<T> currentElement;
+
+  SIterator(Link<T> link) {
+    currentElement = link;
+  }
+
+  public boolean hasNext() {
+    return currentElement.next() != null;
+  }
+
+  public Link<T> next() {
+    currentElement = currentElement.next();
+    return currentElement;
+  }
+
+  public void remove() {
+    if (currentElement.next() != null) {
+      currentElement.setNext(currentElement.next().next());
+    }
+  }
+
+  public void add(T object) {
+    currentElement.setNext(new Link<>(object, currentElement.next()));
+    currentElement = currentElement.next();
+  }
+}
+
+class Exercise8 {
   public static void main(String[] args) {
     SList<String> sList = new SList<>();
     SIterator<String> iterator = sList.iterator();
     iterator.add("Hello");
     iterator.add("world");
+    System.out.println(sList.toString());
+    iterator = sList.iterator();
+    iterator.remove();
+    iterator.remove();
     System.out.println(sList.toString());
   }
 }
