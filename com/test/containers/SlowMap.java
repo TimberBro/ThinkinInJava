4a749e49db8f9a86b9108dac4f3e691c6f3a6a32
Evgeniy Popov <mr.demix@yandex.ru>
2021-09-20 22:53:05 +0300

    Chapter 19 - Exercise 16.

--- com/test/containers/SlowMap.java
+++ com/test/containers/SlowMap.java
@@ -2,8 +2,8 @@ package com.test.containers;
 
 import com.test.util.Countries;
 import java.util.AbstractMap;
+import java.util.AbstractSet;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -31,14 +31,77 @@ public class SlowMap<K, V> extends AbstractMap<K, V> {
     return values.get(keys.indexOf(key));
   }
 
-  public Set<Entry<K, V>> entrySet() {
-    Set<Map.Entry<K, V>> set = new HashSet<>();
-    Iterator<K> ki = keys.iterator();
-    Iterator<V> vi = values.iterator();
-    while (ki.hasNext()) {
-      set.add(new MapEntry<>(ki.next(), vi.next()));
+  private EntrySet entrySet;
+
+  // From HashMap
+  @Override
+  public Set<Map.Entry<K, V>> entrySet() {
+    Set<Map.Entry<K, V>> es;
+    return (es = entrySet) == null ? (entrySet = new EntrySet()) : es;
+  }
+
+  // Create view into the Map to allow modifications
+  class EntrySet extends AbstractSet<Entry<K, V>> {
+
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+      return new Iterator<Map.Entry<K, V>>() {
+        private int index = -1;
+
+        public boolean hasNext() {
+          return index < keys.size() - 1;
+        }
+
+        public Map.Entry<K, V> next() {
+          ++index;
+          return new MapEntry<>(keys.get(index), values.get(index));
+        }
+
+        public void remove() {
+          keys.remove(index);
+          values.remove(index);
+          // Before move to the next key
+          index--;
+        }
+      };
     }
-    return set;
+
+    // Adapted from HashMap
+    public final int size() {
+      return keys.size();
+    }
+
+    // From HashMap
+    public final boolean contains(Object o) {
+      if (!(o instanceof Map.Entry)) {
+        return false;
+      }
+      Map.Entry<K, V> e = (Map.Entry<K, V>) o;
+      K key = e.getKey();
+      if (keys.contains(key)) {
+        return e.equals(new MapEntry<>(key, get(key)));
+      }
+      return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+      if (contains(o)) {
+        MapEntry<K, V> e = (MapEntry<K, V>) o;
+        K key = e.getKey();
+        V val = e.getValue();
+        keys.remove(key);
+        values.remove(val);
+        return true;
+      }
+      return false;
+    }
+
+    @Override
+    public void clear() {
+      keys.clear();
+      values.clear();
+    }
   }
 
   public static void main(String[] args) {
@@ -46,6 +109,8 @@ public class SlowMap<K, V> extends AbstractMap<K, V> {
     m.putAll(Countries.capitals(15));
     System.out.println(m);
     System.out.println(m.get("BULGARIA"));
+    System.out.println(m.entrySet());
+    m.clear();
     System.out.println(m.entrySet());
   }
 }
