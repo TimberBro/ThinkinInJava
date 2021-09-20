e2e33f89d52490fd7eff2be443010faa7d0d794f
Evgeniy Popov <mr.demix@yandex.ru>
2021-09-20 19:32:18 +0300

    Chapter 19 - Exercise 15.

--- com/test/containers/WordCounter.java
+++ com/test/containers/WordCounter.java
@@ -1,14 +1,13 @@
 package com.test.containers;
 
 import com.test.util.TextFile;
+import java.util.Map;
 
 public class WordCounter {
 
   public static void main(String[] args) {
 
-    AssociativeArray<String, Integer> words =
-        new AssociativeArray<>(new TextFile("com\\test\\util\\test.txt", "\\W+").size());
-
+    Map<String, Integer> words = new SlowMap<>();
     for (String s : new TextFile("com\\test\\util\\test.txt", "\\W+")) {
       if (words.get(s) != null) {
         words.put(s, words.get(s) + 1);
