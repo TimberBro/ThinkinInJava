9133956534c1c0bb9c45965ae30c35e0ca22aa79
Evgeniy Popov <mr.demix@yandex.ru>
2021-09-22 00:20:45 +0300

    Chapter 19 - Exercise 19.

--- com/test/containers/WordCounter.java
+++ com/test/containers/WordCounter.java
@@ -7,7 +7,8 @@ public class WordCounter {
 
   public static void main(String[] args) {
 
-    Map<String, Integer> words = new SlowMap<>();
+    // Works fine with SimpleHashMap
+    Map<String, Integer> words = new SimpleHashMap<>();
     for (String s : new TextFile("com\\test\\util\\test.txt", "\\W+")) {
       if (words.get(s) != null) {
         words.put(s, words.get(s) + 1);
