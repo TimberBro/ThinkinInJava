3779f6861c740cce6ff549d2e966115db24048ee
Evgeniy Popov <mr.demix@yandex.ru>
2021-09-23 01:18:34 +0300

    Chapter 19 - Exercise 22. Exercise 21 was accidentally solved in exercise 20.

--- com/test/containers/SimpleHashMap.java
+++ com/test/containers/SimpleHashMap.java
@@ -3,6 +3,7 @@ package com.test.containers;
 import com.test.util.Countries;
 import java.util.AbstractMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Map;
@@ -33,8 +34,8 @@ public class SimpleHashMap<K, V> extends AbstractMap<K, V> {
         oldValue = iPair.getValue();
         it.set(pair); // Replace old with new
         found = true;
-        System.out.println(
-            "Collision was found on key: " + iPair.getKey() + ".\nPrevious value = " + oldValue);
+        // System.out.println("Collision was found on key: " + iPair.getKey() + ".\nPrevious value =
+        // " + oldValue);
         break;
       }
     }
@@ -70,11 +71,40 @@ public class SimpleHashMap<K, V> extends AbstractMap<K, V> {
     return set;
   }
 
+  // Almost all copied from AbstractMap and from get method.
+  @Override
+  public V remove(Object key) {
+    int index = Math.abs(key.hashCode()) % SIZE;
+    if (buckets[index] == null) {
+      return null;
+    }
+    Iterator<MapEntry<K, V>> it = buckets[index].iterator();
+    while (it.hasNext()) {
+      MapEntry<K, V> iPair = it.next();
+      if (iPair.getKey().equals(key)) {
+        V value = iPair.getValue();
+        it.remove();
+        return value;
+      }
+    }
+    return null;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public void clear() {
+    buckets = new LinkedList[SIZE];
+  }
+
   public static void main(String[] args) {
     SimpleHashMap<String, String> m = new SimpleHashMap<>();
     m.putAll(Countries.capitals(25));
     System.out.println(m);
     System.out.println(m.get("ERITREA"));
     System.out.println(m.entrySet());
+    System.out.println(m.remove("ANGOLA"));
+    System.out.println(m);
+    m.clear();
+    System.out.println(m);
   }
 }
