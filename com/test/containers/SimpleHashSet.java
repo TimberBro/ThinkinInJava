0c40222436a2f0933698d2251f6443d773a8a9a4
Evgeniy Popov <mr.demix@yandex.ru>
2021-09-24 00:17:32 +0300

    Chapter 19 - Exercise 24. From solution guide.

---
+++ com/test/containers/SimpleHashSet.java
@@ -0,0 +1,127 @@
+package com.test.containers;
+
+import com.test.util.Countries;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+public class SimpleHashSet<T> extends AbstractSet<T> {
+
+  static final int SIZE = 997;
+
+  @SuppressWarnings("unchecked")
+  LinkedList<T>[] buckets = new LinkedList[SIZE];
+
+  @Override
+  public boolean add(T key) {
+    int index = Math.abs(key.hashCode()) % SIZE;
+    if (buckets[index] == null) {
+      buckets[index] = new LinkedList<>();
+    }
+    LinkedList<T> bucket = buckets[index];
+    ListIterator<T> it = bucket.listIterator();
+    while (it.hasNext()) {
+      if (it.next().equals(key)) {
+        return false;
+      }
+    }
+
+    it.add(key);
+    return true;
+  }
+
+  public boolean contains(Object key) {
+    int index = Math.abs(key.hashCode()) % SIZE;
+    if (buckets[index] == null) {
+      return false;
+    }
+    Iterator<T> it = buckets[index].iterator();
+    while (it.hasNext()) {
+      if (it.next().equals(key)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Need more information about HashSet. Don't understand how to iterate through objects in
+  // hashset.
+  @Override
+  public Iterator<T> iterator() {
+    return new Iterator<T>() {
+      int count;
+      int index1;
+      int index2;
+      boolean canRemove;
+
+      @Override
+      public boolean hasNext() {
+        return count < size();
+      }
+
+      public T next() {
+        if (hasNext()) {
+          canRemove = true;
+          ++count;
+          while (true) {
+            // Position of the next non-empty bucket
+            while (buckets[index1] == null) {
+              ++index1;
+            }
+            // Position of the next item to return
+            try {
+              return buckets[index1].get(index2++);
+            } catch (IndexOutOfBoundsException e) {
+              // Continue search from the next bucket
+              ++index1;
+              index2 = 0;
+            }
+          }
+        }
+        throw new NoSuchElementException();
+      }
+
+      @Override
+      public void remove() {
+        if (canRemove) {
+          canRemove = false;
+          buckets[index1].remove(--index2);
+          if (buckets[index1].isEmpty()) {
+            buckets[index1++] = null;
+            --count;
+          }
+        } else {
+          throw new IllegalStateException();
+        }
+      }
+    };
+  }
+
+  @Override
+  public int size() {
+    int size = 0;
+    for (LinkedList<T> bucket : buckets) {
+      if (bucket != null) {
+        size += bucket.size();
+      }
+    }
+    return size;
+  }
+
+  public static void main(String[] args) {
+    SimpleHashSet<String> m = new SimpleHashSet<>();
+    m.addAll(Countries.names(10));
+    m.addAll(Countries.names(10));
+    System.out.println("m = " + m);
+    System.out.println("m.size() = " + m.size());
+    Iterator<String> it = m.iterator();
+    System.out.println("it.next()= " + it.next());
+    it.remove();
+    System.out.println("it.next()= " + it.next());
+    System.out.println("m = " + m);
+    m.remove("ALGERIA");
+    System.out.println("m = " + m);
+  }
+}
