c2cd572036790da9215f53c8e920d88bc0f0fdf7
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-10-02 01:27:12 +0300

    Chapter 19 - Exercise 37.

--- com/test/containers/SetPerformance.java
+++ com/test/containers/SetPerformance.java
@@ -1,57 +1,55 @@
 package com.test.containers;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class SetPerformance {
-
-
 
   static List<Test<Set<String>>> tests = new ArrayList<>();
 
   static {
-    tests.add(new Test<Set<String>>("add") {
-      int test(Set<String> set, TestParam tp) {
-        int loops = tp.loops;
-        int size = tp.size;
-        for (int i = 0; i < loops; i++) {
-          set.clear();
-          for (int j = 0; j < size; j++) {
-            set.add(Integer.toString(j));
+    tests.add(
+        new Test<Set<String>>("add") {
+          int test(Set<String> set, TestParam tp) {
+            int loops = tp.loops;
+            int size = tp.size;
+            for (int i = 0; i < loops; i++) {
+              set.clear();
+              for (int j = 0; j < size; j++) {
+                set.add(Integer.toString(j));
+              }
+            }
+            return loops * size;
           }
-        }
-        return loops * size;
-      }
-    });
-    tests.add(new Test<Set<String>>("contains") {
-      int test(Set<String> set, TestParam tp) {
-        int loops = tp.loops;
-        int span = tp.size * 2;
-        for (int i = 0; i < loops; i++) {
-          for (int j = 0; j < span; j++) {
-            set.contains(Integer.toString(j));
+        });
+    tests.add(
+        new Test<Set<String>>("contains") {
+          int test(Set<String> set, TestParam tp) {
+            int loops = tp.loops;
+            int span = tp.size * 2;
+            for (int i = 0; i < loops; i++) {
+              for (int j = 0; j < span; j++) {
+                set.contains(Integer.toString(j));
+              }
+            }
+            return loops * span;
           }
-        }
-        return loops * span;
-      }
-    });
-    tests.add(new Test<Set<String>>("iterate") {
-      int test(Set<String> set, TestParam tp) {
-        int loops = tp.loops * 10;
-        for (int i = 0; i < loops; i++) {
-          Iterator<String> it = set.iterator();
-          while (it.hasNext()) {
-            it.next();
+        });
+    tests.add(
+        new Test<Set<String>>("iterate") {
+          int test(Set<String> set, TestParam tp) {
+            int loops = tp.loops * 10;
+            for (int i = 0; i < loops; i++) {
+              Iterator<String> it = set.iterator();
+              while (it.hasNext()) {
+                it.next();
+              }
+            }
+            return loops * set.size();
           }
-        }
-        return loops * set.size();
-      }
-    });
+        });
   }
 
   public static void main(String[] args) {
@@ -59,8 +57,7 @@ public class SetPerformance {
       Tester.defaultParams = TestParam.array(args);
     }
     Tester.fieldWidth = 10;
-    Tester.run(new TreeSet<>(), tests);
-    Tester.run(new HashSet<>(), tests);
-    Tester.run(new LinkedHashSet<>(), tests);
+    Tester.run(new SimpleHashSet<>(), tests);
+    Tester.run(new ArraySimpleHashSet<>(), tests);
   }
 }
