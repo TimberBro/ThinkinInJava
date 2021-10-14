ce66aa6d35e4da9146a66c24ac70c494d7710827
Evgeniy Popov <mr.demix@yandex.ru>
2021-10-14 22:32:49 +0300

    Chapter 20 - Exercise 8.

--- com/test/io/TextFileToLinkedList.java
+++ com/test/io/TextFileToLinkedList.java
@@ -1,7 +1,6 @@
 package com.test.io;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,25 +9,29 @@ import java.util.LinkedList;
 public class TextFileToLinkedList {
 
   public static void main(String[] args) {
-    LinkedList<String> list = new LinkedList<>();
-    try {
-      BufferedReader in = new BufferedReader(new FileReader("com\\test\\util\\Test.txt"));
-      while (true) {
-        String currentLine = in.readLine();
-        if (currentLine != null) {
-          list.add(currentLine);
-        } else {
-          break;
+    if (args.length != 1) {
+      System.out.println("Usage java TextFileToLinkedList %full path to file%");
+    } else {
+      LinkedList<String> list = new LinkedList<>();
+      try {
+        BufferedReader in = new BufferedReader(new FileReader(args[0]));
+        while (true) {
+          String currentLine = in.readLine();
+          if (currentLine != null) {
+            list.add(currentLine);
+          } else {
+            break;
+          }
         }
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
       }
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
 
-    for (int i = list.size() - 1; i >= 0; i--) {
-      System.out.println(list.get(i));
+      for (int i = list.size() - 1; i >= 0; i--) {
+        System.out.println(list.get(i));
+      }
     }
   }
 }
