ae4c2ebca611d69592bfd24cca8cdad955a5d63b
Evgeniy Popov <mr.demix@yandex.ru>
2021-10-28 21:38:36 +0300

    Chapter 20 - Exercise 21.

---
+++ com/test/io/CapitalizeStdInput.java
@@ -0,0 +1,16 @@
+package com.test.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CapitalizeStdInput {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+    String s;
+    while ((s = stdin.readLine()) != null) {
+      System.out.println(s.toUpperCase());
+    }
+  }
+}
