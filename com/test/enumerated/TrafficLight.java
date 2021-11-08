d67393c6a68db9d9e2e0ab1d7804dd98be81890f
Evgeniy Popov <mr.demix@yandex.ru>
2021-11-08 22:15:43 +0300

    Chapter 21 - Exercise 1.

---
+++ com/test/enumerated/TrafficLight.java
@@ -0,0 +1,42 @@
+package com.test.enumerated;
+
+import static com.test.enumerated.Signal.GREEN;
+import static com.test.enumerated.Signal.RED;
+import static com.test.enumerated.Signal.YELLOW;
+
+enum Signal {
+  GREEN, YELLOW, RED
+}
+
+public class TrafficLight {
+
+  Signal color = Signal.RED;
+
+  public void change() {
+    switch (color) {
+      // Note that you don't have to say Signal.RED
+      // in the case statement:
+      case RED:
+        color = GREEN;
+        break;
+      case GREEN:
+        color = YELLOW;
+        break;
+      case YELLOW:
+        color = RED;
+        break;
+    }
+  }
+
+  public String toString() {
+    return "The traffic light is " + color;
+  }
+
+  public static void main(String[] args) {
+    TrafficLight t = new TrafficLight();
+    for (int i = 0; i < 7; i++) {
+      System.out.println(t);
+      t.change();
+    }
+  }
+}
