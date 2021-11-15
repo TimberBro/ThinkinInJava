0e1891249c26a41089fa5510cac2ae607b7d8705
Evgeniy Popov <mr.demix@yandex.ru>
2021-11-15 22:16:43 +0300

    Chapter 23 - Exercise 5.

--- com/test/concurrency/MultithreadedFibonacci.java
+++ com/test/concurrency/MultithreadedFibonacci.java
@@ -1,35 +1,51 @@
 package com.test.concurrency;
 
 import com.test.generics.Fibonacci;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-public class MultithreadedFibonacci implements Runnable {
+public class MultithreadedFibonacci implements Callable<String> {
 
   private final int id = taskCount++;
   private static int taskCount = 0;
   private int sequenceLength;
+  private int sum;
 
   MultithreadedFibonacci(int sequenceLength) {
     this.sequenceLength = sequenceLength;
   }
 
   @Override
-  public void run() {
-    LinkedList<Integer> list = new LinkedList<>();
+  public String call() {
     Fibonacci gen = new Fibonacci();
     for (int i = 0; i < sequenceLength; i++) {
-      list.add(gen.next());
+      sum += gen.next();
     }
-    System.out.println("From Thread№" + id + ": " + list);
+    return "Sum of first " + sequenceLength + " fibonacci numbers from thread№" + id + " = " + sum;
   }
 
   public static void main(String[] args) {
     ExecutorService executor = Executors.newCachedThreadPool();
+    ArrayList<Future<String>> list = new ArrayList<>();
     for (int i = 1; i <= 5; i++) {
-      executor.execute(new MultithreadedFibonacci(i));
+      list.add(executor.submit(new MultithreadedFibonacci(i)));
     }
-    executor.shutdown();
+    for (Future<String> string : list) {
+      try {
+        System.out.println(string.get());
+      } catch (InterruptedException e) {
+        System.out.println(e);
+        return;
+      } catch (ExecutionException e) {
+        System.out.println(e);
+      } finally {
+        executor.shutdown();
+      }
+    }
   }
 }
