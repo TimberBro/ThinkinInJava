7a19ea4e554321870a90519ea1f3997a7dd4d296
Evgeniy Popov <mr.demix@yandex.ru>
2021-11-16 23:44:52 +0300

    Chapter 23 - Exercise 10. runTask() called by hands.

--- com/test/concurrency/MultithreadedFibonacci.java
+++ com/test/concurrency/MultithreadedFibonacci.java
@@ -2,12 +2,12 @@ package com.test.concurrency;
 
 import com.test.generics.Fibonacci;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 public class MultithreadedFibonacci implements Callable<String> {
 
@@ -15,9 +15,17 @@ public class MultithreadedFibonacci implements Callable<String> {
   private static int taskCount = 0;
   private int sequenceLength;
   private int sum;
+  Thread t;
 
-  MultithreadedFibonacci(int sequenceLength) {
+  public Future<String> runTask(int sequenceLength)
+      throws ExecutionException, InterruptedException {
     this.sequenceLength = sequenceLength;
+    FutureTask<String> futureTask = new FutureTask<>(this);
+    if (t == null) {
+      t = new Thread(futureTask);
+      t.start();
+    }
+    return futureTask;
   }
 
   @Override
@@ -33,7 +41,13 @@ public class MultithreadedFibonacci implements Callable<String> {
     ExecutorService executor = Executors.newCachedThreadPool();
     ArrayList<Future<String>> list = new ArrayList<>();
     for (int i = 1; i <= 5; i++) {
-      list.add(executor.submit(new MultithreadedFibonacci(i)));
+      try {
+        list.add(new MultithreadedFibonacci().runTask(i));
+      } catch (ExecutionException e) {
+        e.printStackTrace();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
     for (Future<String> string : list) {
       try {
