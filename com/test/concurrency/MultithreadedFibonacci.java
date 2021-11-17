73ee1e07a237b92241036d329910a859628f117e
Evgeniy Popov <mr.demix@yandex.ru>
2021-11-17 10:44:10 +0300

    Chapter 23 - Exercise 10.

--- com/test/concurrency/MultithreadedFibonacci.java
+++ com/test/concurrency/MultithreadedFibonacci.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
 
 public class MultithreadedFibonacci implements Callable<String> {
 
@@ -15,17 +14,11 @@ public class MultithreadedFibonacci implements Callable<String> {
   private static int taskCount = 0;
   private int sequenceLength;
   private int sum;
-  Thread t;
+  ExecutorService exec = Executors.newCachedThreadPool();
 
-  public Future<String> runTask(int sequenceLength)
-      throws ExecutionException, InterruptedException {
+  public Future<String> runTask(int sequenceLength) {
     this.sequenceLength = sequenceLength;
-    FutureTask<String> futureTask = new FutureTask<>(this);
-    if (t == null) {
-      t = new Thread(futureTask);
-      t.start();
-    }
-    return futureTask;
+    return exec.submit(this);
   }
 
   @Override
@@ -37,29 +30,19 @@ public class MultithreadedFibonacci implements Callable<String> {
     return "Sum of first " + sequenceLength + " fibonacci numbers from thread№" + id + " = " + sum;
   }
 
+  // Solution possibly not multithreaded. Not enough information to solve.
   public static void main(String[] args) {
-    ExecutorService executor = Executors.newCachedThreadPool();
-    ArrayList<Future<String>> list = new ArrayList<>();
-    for (int i = 1; i <= 5; i++) {
-      try {
-        list.add(new MultithreadedFibonacci().runTask(i));
-      } catch (ExecutionException e) {
-        e.printStackTrace();
-      } catch (InterruptedException e) {
-        e.printStackTrace();
+    MultithreadedFibonacci fibonacci = new MultithreadedFibonacci();
+    try {
+      for (int i = 1; i <= 5; i++) {
+        System.out.println(fibonacci.runTask(i).get());
       }
-    }
-    for (Future<String> string : list) {
-      try {
-        System.out.println(string.get());
-      } catch (InterruptedException e) {
-        System.out.println(e);
-        return;
-      } catch (ExecutionException e) {
-        System.out.println(e);
-      } finally {
-        executor.shutdown();
-      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    } finally {
+      fibonacci.exec.shutdown();
     }
   }
 }
