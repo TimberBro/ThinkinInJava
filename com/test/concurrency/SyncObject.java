83123a789c1f4dd8035731928ba8898e2c4fa425
desktop-epopov\epopov <mr.demix@yandex.ru>
2021-11-25 23:29:44 +0300

    Chapter 23 - Exercise 16.

--- com/test/concurrency/SyncObject.java
+++ com/test/concurrency/SyncObject.java
@@ -1,33 +1,46 @@
 package com.test.concurrency;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 class DualSynch {
   private Object syncObject = new Object();
   private Object syncObject1 = new Object();
+  Lock lock = new ReentrantLock();
 
-  public synchronized void f() {
-    for (int i = 0; i < 5; i++) {
-      System.out.println("f()");
-      Thread.yield();
+  public void f() {
+    lock.lock();
+    try {
+      for (int i = 0; i < 5; i++) {
+        System.out.println("f()");
+        Thread.yield();
+      }
+    } finally {
+      lock.unlock();
     }
   }
 
   public void g() {
-    // Should be synchronized using this keyword
-    synchronized (syncObject) {
+    lock.lock();
+    try {
       for (int i = 0; i < 5; i++) {
         System.out.println("g()");
         Thread.yield();
       }
+    } finally {
+      lock.unlock();
     }
   }
 
   public void k() {
-    // Should be synchronized using this keyword
-    synchronized (syncObject1) {
+    lock.lock();
+    try {
       for (int i = 0; i < 5; i++) {
         System.out.println("k()");
         Thread.yield();
       }
+    } finally {
+      lock.unlock();
     }
   }
 }
@@ -36,6 +49,7 @@ public class SyncObject {
   public static void main(String[] args) {
     final DualSynch ds = new DualSynch();
     new Thread() {
+      @Override
       public void run() {
         ds.f();
       }
